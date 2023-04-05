24b2a89094db404612fc095d6f0496f9ddf1cb78
Shailesh Shivam <shaileshshivam.dev@gmail.com>
2023-04-05 12:27:19 +0530

    fix typo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const address = "0xB2a0449699876824CC16121dD19195E821A5956B"
 const transactionHash = "0x1599ea8a9866a1c6d435c7a3b1443cd31273a8835c65a2c4ad05dbf8af3e51f7"
 
 async function getTransactionReceipt(hash) {
-    const receipt = await alchemy.core.getTransactionReceipt(transactionHash)
+    const receipt = await alchemy.core.getTransactionReceipt(hash)
     const gasUsed = receipt.gasUsed
     const effectiveGasPrice = receipt.effectiveGasPrice
 
